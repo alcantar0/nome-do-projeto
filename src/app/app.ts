44c4fce254d79a0c27fbb0f6f21471b7dc6e35d8
Pedro Henrique <alcantarapedro69@yahoo.com>
2025-06-05 17:48:59 -0300

    ok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,80 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // 👈 Importação aqui
+import { Console } from 'console';
+
+
+
+export interface Cliente{
+  nome: string;
+  tipo: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+
+
+export class App implements OnInit {
   protected title = 'nome-do-projeto';
+
+  nome_cliente = '';
+  tipo_cliente = 'geral'; // valor padrão
+
+  resposta: Cliente[] = [];
+  
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit() {
+    this.fazerRequisicao();
+    
+  }
+  fazerRequisicao() {
+const url = 'http://localhost:8080/api/fila';
+
+this.http.get<Cliente[]>(url).subscribe({
+  next: (dados) => {
+    this.resposta = dados;
+  },
+  error: (erro) => {
+    console.error('Erro na requisição:', erro);
+  }
+});
+}
+
+adicionarPaciente() {
+  console.log('submit!')
+  console.log('Chamando adicionarPaciente()');
+
+  const url = 'http://localhost:8080/api/fila';
+  const novoCliente: Cliente = {
+    nome: this.nome_cliente,
+    tipo: this.tipo_cliente
+  };
+  console.log('Enviando:', novoCliente); // 👈 veja se nome/tipo estão corretos
+
+  this.http.post<Cliente>(url, novoCliente).subscribe({
+    next: () => this.fazerRequisicao(),
+    error: (erro) => {
+      console.error('Erro ao atender paciente:', erro);
+    }
+  });
+}
+
+atenderPaciente() {
+  const url = 'http://localhost:8080/api/fila/atender';
+  this.http.delete(url).subscribe({
+    next: () => this.fazerRequisicao(),
+    error: (erro) => {
+      console.error('Erro ao atender paciente:', erro);
+    }
+  });
+}
 }
